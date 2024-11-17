2be5ce1201eb7a0aacccd40dac6f7d671f04ebbd
codebash <agarwalsabhya44@gmail.com>
2024-11-18 02:04:13 +0530

    project component correction

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -34,12 +34,12 @@ export default function ProjectCard({
 			<div className="mt-2 flex items-center justify-between gap-4">
 				<Link href={githubLink} target="_blank">
 					<button className="md:text-md inline-flex w-fit animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-sm font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-						Visit Live Site
+						Code
 					</button>
 				</Link>
 				<Link href={websiteLink} target="_blank">
 					<button className="md:text-md inline-flex w-fit animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-sm font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-						Code
+						Visit Live Site
 					</button>
 				</Link>
 			</div>
